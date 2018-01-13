6b73e499738778da00d1aefc10628f7fed3c18a7
Rubiooo <maplerubiooo>
2018-01-13 01:09:47 -0500

    view_pager_with fragment_with_tab.

---
+++ app/src/main/java/com/example/rubioo/myviewpagerpractice/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.rubioo.myviewpagerpractice;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.view_pager);
+        viewPager.setAdapter(new NumberPagerAdapter(getSupportFragmentManager()));
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.view_pager_tab);
+
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    private class NumberPagerAdapter extends FragmentPagerAdapter {
+        public NumberPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return PageFragment.newInstance(position);
+        }
+
+        @Override
+        public int getCount() {
+            return 4;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return "page" + position;
+        }
+    }
+}
