dfc349309b1ece3153d5c55c8e0ab7e42d278d77
Rubiooo <maplerubiooo>
2018-01-12 23:35:25 -0500

    view_pager_with fragment

---
+++ app/src/main/java/com/example/rubioo/myviewpagerpractice/PageFragment.java
@@ -0,0 +1,47 @@
+package com.example.rubioo.myviewpagerpractice;
+
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by Jimmy on 2018/1/12.
+ */
+
+public class PageFragment extends Fragment {
+
+    public static final String KEY_PAGE = "page";
+    private TextView textView;
+
+    @NonNull
+    public static PageFragment newInstance(int page) {
+        Bundle args = new Bundle();
+        args.putInt(KEY_PAGE, page);
+
+        PageFragment pageFragment = new PageFragment();
+        pageFragment.setArguments(args);
+        return pageFragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.page_fragment, container, false);
+        textView = (TextView) view.findViewById(R.id.text_view);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        int page =  getArguments().getInt(KEY_PAGE);
+        textView.setText("page" + page);
+    }
+}
